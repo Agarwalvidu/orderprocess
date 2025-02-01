4a5670e08032201535b8166c31aef38ea63a98e2
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2025-02-02 00:15:26 +0530

    commit 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Dashboard } from "./components/dashboard/dash";
 import { CustomerOrders } from "./components/sustain/sustain";
+import { Inventory } from "./components/Inventory/inventory";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/sustain" element={<CustomerOrders />} />
+      <Route path="/inventory" element={<Inventory />} />
     </Routes>
   </BrowserRouter>
   );
