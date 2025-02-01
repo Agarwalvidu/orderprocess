912882972c391f466764c4e393c0a2270c0b9da8
Shefali Kaushal <shefali2404kaushal@gmail.com>
2025-02-01 22:43:09 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
-import { Dashboard } from "./components/dashboard/dash";
+import { Dashboard } from "./components/Dashboard/dash";
+import {Inventory} from "./components/Inventory/inventory";
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/inventory" element={<Inventory />} />
     </Routes>
   </BrowserRouter>
   );
