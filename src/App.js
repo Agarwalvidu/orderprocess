a0b7fc59a4c88674dd6bb533feace5fcd27699f6
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2025-02-02 02:03:46 +0530

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { Dashboard } from "./components/dashboard/dash";
 import { CustomerOrders } from "./components/sustain/sustain";
 import { Inventory } from "./components/Inventory/inventory";
+import Layout from "./components/dashboard/toggle";
 
 function App() {
   return (
     <BrowserRouter>
+    <Layout>
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/sustain" element={<CustomerOrders />} />
       <Route path="/inventory" element={<Inventory />} />
     </Routes>
+    </Layout>
   </BrowserRouter>
   );
 }
